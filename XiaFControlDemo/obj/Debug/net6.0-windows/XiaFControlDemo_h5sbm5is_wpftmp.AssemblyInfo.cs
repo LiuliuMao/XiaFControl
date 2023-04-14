b6c533c2e9174ef897f03201c2d3b335f2decfd8
1396744546 <1396744546@qq.com>
2023-04-14 09:12:19 +0800

    1

--- XiaFControlDemo/obj/Debug/net6.0-windows/XiaFControlDemo_h5sbm5is_wpftmp.AssemblyInfo.cs
+++ XiaFControlDemo/obj/Debug/net6.0-windows/XiaFControlDemo_h5sbm5is_wpftmp.AssemblyInfo.cs
@@ -11,16 +11,13 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("LiuliuMao")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("XiaFControlDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyDescriptionAttribute("XiaF is a control library applied to WPF, currently supporting. Will be continuou" +
-    "sly updated.")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
-[assembly: System.Reflection.AssemblyProductAttribute("XiaFControl")]
-[assembly: System.Reflection.AssemblyTitleAttribute("XiaFControl")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
-[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/LiuliuMao/XiaFControl.git")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("XiaFControlDemo")]
+[assembly: System.Reflection.AssemblyTitleAttribute("XiaFControlDemo")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
