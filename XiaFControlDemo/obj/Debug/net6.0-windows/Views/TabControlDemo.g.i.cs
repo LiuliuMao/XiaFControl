eb646840aeaeb92f444718a661dc43ea2c6df660
1396744546 <1396744546@qq.com>
2023-04-18 15:47:03 +0800

    1

--- XiaFControlDemo/obj/Debug/net6.0-windows/Views/TabControlDemo.g.i.cs
+++ XiaFControlDemo/obj/Debug/net6.0-windows/Views/TabControlDemo.g.i.cs
@@ -55,7 +55,7 @@ namespace XiaFControlDemo.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/XiaFControlDemo;V1.0.0.0;component/views/tabcontroldemo.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/XiaFControlDemo;component/views/tabcontroldemo.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\TabControlDemo.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
