a7fc95eabd940e9fd2aad0c437e02701ef73e5d1
1396744546 <1396744546@qq.com>
2023-04-23 16:45:22 +0800

    1

--- XiaFControlDemo/obj/Debug/net6.0-windows/Views/ListsTreeDemo.g.i.cs
+++ XiaFControlDemo/obj/Debug/net6.0-windows/Views/ListsTreeDemo.g.i.cs
@@ -72,7 +72,7 @@ namespace XiaFControlDemo.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/XiaFControlDemo;component/views/liststreedemo.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/XiaFControlDemo;V1.0.0.0;component/views/liststreedemo.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\ListsTreeDemo.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
