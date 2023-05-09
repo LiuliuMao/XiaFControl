81dac99fe1b4a0c6334d5f047378b0119925d729
1396744546 <1396744546@qq.com>
2023-05-09 14:34:35 +0800

    1

--- XiaFControlDemo/obj/Debug/net6.0-windows/Views/DateTimeControlDemo.g.i.cs
+++ XiaFControlDemo/obj/Debug/net6.0-windows/Views/DateTimeControlDemo.g.i.cs
@@ -55,7 +55,7 @@ namespace XiaFControlDemo.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/XiaFControlDemo;V1.0.0.0;component/views/datetimecontroldemo.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/XiaFControlDemo;component/views/datetimecontroldemo.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\DateTimeControlDemo.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
