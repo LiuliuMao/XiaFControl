afe72a830f1df7fe738c83384ef82d303efada5e
1396744546 <1396744546@qq.com>
2023-05-30 17:39:09 +0800

    1

--- XiaFControlDemo/obj/Debug/net6.0-windows/Views/InputBoxDemo.g.i.cs
+++ XiaFControlDemo/obj/Debug/net6.0-windows/Views/InputBoxDemo.g.i.cs
@@ -55,7 +55,7 @@ namespace XiaFControlDemo.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/XiaFControlDemo;component/views/inputboxdemo.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/XiaFControlDemo;V1.0.0.0;component/views/inputboxdemo.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\InputBoxDemo.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- XiaFControlDemo/obj/Debug/net6.0-windows/Views/SelectBoxDemo.g.i.cs
+++ XiaFControlDemo/obj/Debug/net6.0-windows/Views/SelectBoxDemo.g.i.cs
@@ -71,7 +71,7 @@ namespace XiaFControlDemo.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/XiaFControlDemo;component/views/selectboxdemo.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/XiaFControlDemo;V1.0.0.0;component/views/selectboxdemo.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\SelectBoxDemo.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
